5ef40ce7c791abde1afe155479c6df1d6189eecd
Ignacio - Innovo <ignacioelfi@gmail.com>
2020-03-18 21:33:39 -0300

    Initial version.

---
+++ MDBSaas/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MDBSaas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.RenderFullPageIntro = true;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.RenderFullPageIntro = false;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.RenderFullPageIntro = false;
+            return View();
+        }
+
+        public ActionResult Pricing()
+        {
+            ViewBag.RenderFullPageIntro = false;
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# MDBSaas
-MDB Saas Template in ASP.NET MVC
+# MDB Saas Template in ASP.NET MVC
+
+Integrating MDBootstrap in ASP.NET MVC
+
+Here you will find a working project to take as starting point.
+
+Note that it includes a conditional attribute in "header" in order not to show the full height when the Carousel is not rendered (indicated by ViewBag.RenderCarousel).
+
+It includes Pricing, About and Contact Pages.
+
+Thanks to the MDBootstrap Team.
